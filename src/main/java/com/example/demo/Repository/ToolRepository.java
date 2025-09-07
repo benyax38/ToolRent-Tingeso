b5e71804aac9e5267144e082b4d614a087f8d22f
benya <benjamin.sandoval.b@usach.cl>
2025-09-07 02:37:13 -0300

    Agregada la carpeta de frontend dentro del proyecto

--- src/main/java/com/example/demo/Repository/ToolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Entity.ToolEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToolRepository extends JpaRepository<ToolEntity,Long> {
-}
